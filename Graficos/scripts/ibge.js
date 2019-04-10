26f6ba198643b2a090c60593eb2c68af1700a8ab
Baita - Desgin Estratégia e Desenvolvimento <baita.repos@gmail.com>
2019-04-10 11:19:17 -0300

    ChartJS Learn pt.2

--- Graficos/scripts/ibge.js
+++ Graficos/scripts/ibge.js
@@ -1,7 +1,7 @@
 //Obtéms os dados referentes às regiões Norte (1), Nordeste (2), Sudeste (3), Sul (4) e Centro-Oeste (5)
 function projecaoLocalidade(codigo) {
 
-    let result = "null";
+    let result = null;
     $.ajax({
         type: "GET",
         url: "https://servicodados.ibge.gov.br/api/v1/projecoes/populacao/" + codigo,
@@ -13,17 +13,16 @@ function projecaoLocalidade(codigo) {
             result = data;
         },
         error: function (data) {
-            // console.log(data);
+            console.log(data);
         }
     });
     return result;
 }
 
-
-//Obtéms os dados referentes às regiões Norte (1), Nordeste (2), Sudeste (3), Sul (4) e Centro-Oeste (5)
+//Retorna o ranking dos 20 nomes mais populares do Brasil por década
 function nomesRanking(decada = 2010) {
 
-    let result = "null";
+    let result = null;
     $.ajax({
         type: "GET",
         url: "https://servicodados.ibge.gov.br/api/v2/censos/nomes/ranking?decada="+decada,
@@ -35,8 +34,31 @@ function nomesRanking(decada = 2010) {
             result = data[0].res;
         },
         error: function (data) {
-            // console.log(data);
+             console.log(data);
         }
     });
     return result;
 }
+
+function nomesFrequencia(nome = 'Maria') {
+    let result = null;
+    $.ajax({
+        type: "GET",
+        url: "https://servicodados.ibge.gov.br/api/v2/censos/nomes/"+nome,
+        contentType: "application/json; charset=utf-8",
+        data: "{}",
+        async: false,
+        dataType: "json",
+        success: function (data) {
+            result = {
+                "nome": data[0].nome,
+                "data": data[0].res
+            }
+        },
+        error: function (data) {
+             console.log(data);
+        }
+    });
+    return result;
+}
+
